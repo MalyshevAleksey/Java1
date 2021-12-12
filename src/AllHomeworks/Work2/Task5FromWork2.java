1be1a44ccafae9b29c3755be0b594f62ebca5a29
MalyshevAleksey <prostobrotish@gmail.com>
2021-12-12 14:42:42 +0400

    Update task 5 from work2

--- src/AllHomeworks/Work2/Task5FromWork2.java
+++ src/AllHomeworks/Work2/Task5FromWork2.java
@@ -6,7 +6,6 @@ public class Task5FromWork2 {
     public static void main(String[] args) {
         Task1FromWork2 applicationTask1 = new Task1FromWork2();
         Task4FromWork2 applicationTask4 = new Task4FromWork2();
-        int maxIndex = 0, minIndex = 0;
         double average = 0;
         int length = 5;
         int rndRangeMin = -50;
@@ -15,28 +14,42 @@ public class Task5FromWork2 {
         applicationTask1.printIntArray(array);
         int max = isMax(array);
         int min = isMin(array);
-        for (int i = 0; i < array.length; i++) {
-            if (array[i]==max) {
-                max=array[i];
-                maxIndex = i;
-            }
-        }
-        for (int i = 0; i < array.length; i++) {
-            if (array[i]==min) {
-                min=array[i];
-                minIndex = i;
-            }
-        }
+        int maxIndex = getIndex(array, max);
+        int minIndex = getIndex(array, min);
         System.out.print("\n");
-        if (minIndex > maxIndex) {
-            minIndex += maxIndex;
-            maxIndex = minIndex - maxIndex;
-            minIndex -= maxIndex;
+        int [] correctInterval = determiningCorrectInterval(minIndex, maxIndex);
+        System.out.println("Interval (indexes): "+ correctInterval[0]+" "+correctInterval[1]);
+        int lenthInterval = correctInterval[1] - correctInterval[0];
+        average = averageInterval(correctInterval, array, lenthInterval);
+        System.out.println("Average interval: " + average);
+    }
+    public static int getIndex(int [] array, int number) {
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]==number) {
+                number=array[i];
+                index = i;
+            }
         }
-        for (int i = minIndex; i <= maxIndex; i++) {
+        return index;
+    }
+    public static int[] determiningCorrectInterval(int numberOne, int numberTwo) {
+        int [] correctInterval = new int[2];
+        if (numberOne > numberTwo) {
+            numberOne += numberTwo;
+            numberTwo = numberOne - numberTwo;
+            numberOne -= numberTwo;
+        }
+        correctInterval[0] = numberOne;
+        correctInterval[1] = numberTwo;
+        return correctInterval;
+    }
+    public static double averageInterval(int[] correctInterval, int[] array, int lenthInterval) {
+        double average = 0;
+        for (int i = correctInterval[0]; i <= correctInterval[1]; i++) {
             average += array[i];
         }
-        average /= maxIndex - minIndex + 1;
-        System.out.println(average);
+        average /= lenthInterval + 1;
+        return average;
     }
 }
